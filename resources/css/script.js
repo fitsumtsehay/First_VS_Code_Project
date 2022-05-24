36a931e45deb12a7b5d8ec87d2634f30f8afff55
Fitsum Tsehay <tsehayfitsum@gmail.com>
2022-05-24 02:51:05 -0400

    I bootstrapified and made chagenges in about and css file

--- resources/css/script.js
+++ resources/css/script.js
@@ -1,42 +1,40 @@
-  // search-box open close js code
-  let navbar = document.querySelector(".navbar");
-  let searchBox = document.querySelector(".search-box .bx-search");
-  // let searchBoxCancel = document.querySelector(".search-box .bx-x");
-  
-  searchBox.addEventListener("click", ()=>{
-    navbar.classList.toggle("showInput");
-    if(navbar.classList.contains("showInput")){
-      searchBox.classList.replace("bx-search" ,"bx-x");
-    }else {
-      searchBox.classList.replace("bx-x" ,"bx-search");
-    }
-  });
-  
-  // sidebar open close js code
-  let navLinks = document.querySelector(".nav-links");
-  let menuOpenBtn = document.querySelector(".navbar .bx-menu");
-  let menuCloseBtn = document.querySelector(".nav-links .bx-x");
-  menuOpenBtn.onclick = function() {
-  navLinks.style.left = "0";
-  }
-  menuCloseBtn.onclick = function() {
-  navLinks.style.left = "-100%";
-  }
-  
-  
-  // sidebar submenu open close js code
-  let htmlcssArrow = document.querySelector(".htmlcss-arrow");
-  htmlcssArrow.onclick = function() {
-   navLinks.classList.toggle("show1");
-  }
-  let moreArrow = document.querySelector(".more-arrow");
-  moreArrow.onclick = function() {
-   navLinks.classList.toggle("show2");
-  }
-  let jsArrow = document.querySelector(".js-arrow");
-  jsArrow.onclick = function() {
-   navLinks.classList.toggle("show3");
-  }
 
-  
-  
+window.addEventListener('resize', function(){
+    addRequiredClass();
+})
+
+
+function addRequiredClass() {
+    if (window.innerWidth < 860) {
+        document.body.classList.add('mobile')
+    } else {
+        document.body.classList.remove('mobile') 
+    }
+}
+
+window.onload = addRequiredClass
+
+let hamburger = document.querySelector('.hamburger')
+let mobileNav = document.querySelector('.nav-list')
+
+let bars = document.querySelectorAll('.hamburger span')
+
+let isActive = false
+
+hamburger.addEventListener('click', function() {
+    mobileNav.classList.toggle('open')
+    if(!isActive) {
+        bars[0].style.transform = 'rotate(45deg)'
+        bars[1].style.opacity = '0'
+        bars[2].style.transform = 'rotate(-45deg)'
+        isActive = true
+    } else {
+        bars[0].style.transform = 'rotate(0deg)'
+        bars[1].style.opacity = '1'
+        bars[2].style.transform = 'rotate(0deg)'
+        isActive = false
+    }
+    
+
+})
+
